81fb2842edc80520624bc0007a4573cdbeed8a7a
Hassie <38690898+hassiexx@users.noreply.github.com>
2024-12-03 16:40:16 +0000

    Day 2 Part 2

--- 02/main.go
+++ 02/main.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
 
 func main() {
 	part1()
+	part2()
 }
 
 func part1() {
@@ -19,30 +21,62 @@ func part1() {
 	var count int32
 
 	for _, report := range reports {
-		ascending := report[1] > report[0]
-		safe := true
-
-		for i := 1; i < len(report); i++ {
-			diff := math.Abs(float64(report[i]) - float64(report[i-1]))
-			if diff < 1 || diff > 3 {
-				safe = false
-				break
-			}
-
-			if (report[i] > report[i-1] && ascending) || (report[i] < report[i-1] && !ascending) {
-				continue
-			}
-
-			safe = false
-			break
-		}
-
-		if safe {
+		if isSafe(report) {
 			count++
 		}
 	}
 
-	fmt.Println("Safe reports:", count)
+	fmt.Println("Safe reports (part 1):", count)
+}
+
+func part2() {
+	reports := readInput()
+
+	var count int32
+
+	for _, report := range reports {
+		if isSafe(report) {
+			count++
+			continue
+		}
+
+		dampenIndex := 0
+		for {
+			dampenedReport := slices.Clone(report)
+			dampenedReport = append(dampenedReport[:dampenIndex], dampenedReport[dampenIndex+1:]...)
+
+			if isSafe(dampenedReport) {
+				count++
+				break
+			}
+
+			dampenIndex = dampenIndex + 1
+			if dampenIndex == len(report) {
+				break
+			}
+		}
+	}
+
+	fmt.Println("Safe reports (part 1):", count)
+}
+
+func isSafe(report []int8) bool {
+	ascending := report[1] > report[0]
+
+	for i := 1; i < len(report); i++ {
+		diff := math.Abs(float64(report[i]) - float64(report[i-1]))
+		if diff < 1 || diff > 3 {
+			return false
+		}
+
+		if (report[i] > report[i-1] && ascending) || (report[i] < report[i-1] && !ascending) {
+			continue
+		}
+
+		return false
+	}
+
+	return true
 }
 
 func readInput() [][]int8 {
