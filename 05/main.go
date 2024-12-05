62a3fd835dc5271c46e46ce733158f6a3de0866e
Hassie <38690898+hassiexx@users.noreply.github.com>
2024-12-05 17:27:55 +0000

    Day 5 Part 2, refactoring to use slices sort functions

--- 05/main.go
+++ 05/main.go
@@ -5,15 +5,76 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
 
 func main() {
 	part1()
+	part2()
 }
 
 func part1() {
+	rules, updates := readInput()
+
+	var result int32
+
+	for _, update := range updates {
+		if !slices.IsSortedFunc(update, sortFunc(rules)) {
+			continue
+		}
+
+		mid := update[int32(math.Round(float64(len(update)/2)))]
+		midI, err := strconv.ParseInt(mid, 10, 32)
+		if err != nil {
+			panic(fmt.Errorf("failed to parse page number: %w", err))
+		}
+
+		result += int32(midI)
+	}
+
+	fmt.Println("Result (part 1):", result)
+}
+
+func part2() {
+	rules, updates := readInput()
+
+	var result int32
+
+	for _, update := range updates {
+		if slices.IsSortedFunc(update, sortFunc(rules)) {
+			continue
+		}
+
+		slices.SortStableFunc(update, sortFunc(rules))
+
+		mid := update[int32(math.Round(float64(len(update)/2)))]
+		midI, err := strconv.ParseInt(mid, 10, 32)
+		if err != nil {
+			panic(fmt.Errorf("failed to parse page number: %w", err))
+		}
+
+		result += int32(midI)
+	}
+
+	fmt.Println("Result (part 2)", result)
+}
+
+func sortFunc(rules map[string]struct{}) func(a string, b string) int {
+	return func(a, b string) int {
+		k := a + "|" + b
+
+		if _, ok := rules[k]; ok {
+			return -1
+		}
+
+		return 1
+	}
+}
+
+// Alternative solution to part 1 not using built in slice sort func.
+func part1Alternative() {
 	rules, updates := readInput()
 
 	var result int32
